06a554dfaca8c2719b8d55c7d751c575fb2128a6
BuildTools <unconfigured@null.spigotmc.org>
2024-03-03 13:52:22 +0200

    Updated CinemaRoomDTO to work with SeatDTO

--- src/main/java/cinemaroommanager/dto/CinemaRoomDTO.java
+++ src/main/java/cinemaroommanager/dto/CinemaRoomDTO.java
@@ -1,13 +1,17 @@
 package cinemaroommanager.dto;
 
 import cinemaroommanager.model.CinemaRoom;
-import cinemaroommanager.model.Seat;
 
 import java.util.ArrayList;
 
-public record CinemaRoomDTO(int rows, int columns, ArrayList<Seat> seats) {
+public record CinemaRoomDTO(int rows, int columns, ArrayList<SeatDTO> seats) {
 
     public CinemaRoomDTO(CinemaRoom cinemaRoom) {
-        this(cinemaRoom.getNumberOfRows(), cinemaRoom.getNumberOfColumns(), cinemaRoom.getSeats());
+        this(cinemaRoom.getNumberOfRows(),
+                cinemaRoom.getNumberOfColumns(),
+                new ArrayList<>(cinemaRoom.getSeats()
+                        .stream()
+                        .map(SeatDTO::new)
+                        .toList()));
     }
 }
