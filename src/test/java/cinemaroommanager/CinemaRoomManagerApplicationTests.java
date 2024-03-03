bcf9677debacc4542f740c78c195fb96e986cfe0
BuildTools <unconfigured@null.spigotmc.org>
2024-03-03 14:54:01 +0200

    Added tests for POST /purchase endpoint(2/4)

--- src/test/java/cinemaroommanager/CinemaRoomManagerApplicationTests.java
+++ src/test/java/cinemaroommanager/CinemaRoomManagerApplicationTests.java
@@ -1,7 +1,10 @@
 package cinemaroommanager;
 
 import cinemaroommanager.dto.CinemaRoomDTO;
+import cinemaroommanager.dto.SeatDTO;
+import cinemaroommanager.exception.SeatPurchaseException;
 import cinemaroommanager.model.CinemaRoom;
+import cinemaroommanager.model.Seat;
 import cinemaroommanager.service.CinemaService;
 
 import org.hamcrest.Matchers;
@@ -12,12 +15,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -47,6 +52,54 @@ class CinemaRoomManagerApplicationTests {
                 .andExpect(jsonPath("$.seats[:9].row")
                         .value(Matchers.equalTo(List.of(1, 1, 1, 1, 1, 1, 1, 1, 1))))
                 .andExpect(jsonPath("$.seats[?(@.column > 0 && @.column < 10)]")
+                        .value(Matchers.hasSize(81)))
+                .andExpect(jsonPath("$.seats[?(@.price == 10 || @.price == 8)]")
                         .value(Matchers.hasSize(81)));
+    }
+
+    @Test
+    @DisplayName("Test for POST /purchase endpoint")
+    void testEndpointPurchase() throws Exception {
+        var mockSeatDTO = new SeatDTO(new Seat(1, 1));
+        when(cinemaService.purchaseSeat(new SeatDTO(1, 1))).thenReturn(mockSeatDTO);
+
+        var requestBuilder = post("/purchase")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"row\":1,\n\"column\":1}");
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.row").value(1))
+                .andExpect(jsonPath("$.column").value(1))
+                .andExpect(jsonPath("$.price").value(10));
+    }
+
+    @Test
+    @DisplayName("Test for POST /purchase endpoint(with row out of bounds)")
+    void testEndpointPurchaseOutOfBoundsException() throws Exception {
+        when(cinemaService.purchaseSeat(new SeatDTO(15, 1)))
+                .thenThrow(new SeatPurchaseException("The number of a row or a column is out of bounds!"));
+
+        var requestBuilder = post("/purchase")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"row\":15,\n\"column\":1}");
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error")
+                        .value("The number of a row or a column is out of bounds!"));
+    }
+
+    @Test
+    @DisplayName("Test for POST /purchase endpoint(when ticket is purchased)")
+    void testEndpointPurchasePurchasedException() throws Exception {
+        when(cinemaService.purchaseSeat(new SeatDTO(1, 1)))
+                .thenThrow(new SeatPurchaseException("The ticket has been already purchased!"));
+
+        var requestBuilder = post("/purchase")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"row\":1,\n\"column\":1}");
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error")
+                        .value("The ticket has been already purchased!"));
     }
 }
