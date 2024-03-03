5041d481a1331572d9aac19c37b53f306f55f3c5
BuildTools <unconfigured@null.spigotmc.org>
2024-03-03 13:47:36 +0200

    Updated Seat for to work with SeatDTO and added price and isPurchased variables

--- src/main/java/cinemaroommanager/model/Seat.java
+++ src/main/java/cinemaroommanager/model/Seat.java
@@ -3,11 +3,20 @@ package cinemaroommanager.model;
 public class Seat {
     private final int row;
     private final int column;
+    private final int price;
     private boolean isPurchased;
 
     public Seat(int row, int column) {
         this.column = column;
         this.row = row;
+        price = row < 5 ? 10 : 8;
+        isPurchased = false;
+    }
+
+    public Seat(SeatDTO seatDTO) {
+        row = seatDTO.row();
+        column = seatDTO.column();
+        price = row < 5 ? 10 : 8;
     }
 
     public int getRow() {
@@ -16,5 +25,17 @@ public class Seat {
 
     public int getColumn() {
         return column;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public boolean isPurchased() {
+        return isPurchased;
+    }
+
+    public void purchase() {
+        isPurchased = true;
     }
 }
