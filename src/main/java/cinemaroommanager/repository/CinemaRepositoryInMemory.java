425993b2cca35679704aec724abce57444ae526b
BuildTools <unconfigured@null.spigotmc.org>
2024-03-13 14:41:38 +0200

    feat: added CinemaRepository

---
+++ src/main/java/cinemaroommanager/repository/CinemaRepositoryInMemory.java
@@ -0,0 +1,53 @@
+package cinemaroommanager.repository;
+
+import cinemaroommanager.configuration.CinemaConfig;
+import cinemaroommanager.model.Seat;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public class CinemaRepositoryInMemory {
+    private final int columns;
+    private final int rows;
+    private final ArrayList<Seat> seats;
+
+    public CinemaRepositoryInMemory(CinemaConfig cinemaConfig) {
+        columns = cinemaConfig.getNumberOfColumns();
+        rows = cinemaConfig.getNumberOfRows();
+        seats = new ArrayList<>(columns * rows);
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                seats.add(new Seat(i, j));
+            }
+        }
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public ArrayList<Seat> getSeats() {
+        return new ArrayList<>(seats);
+    }
+
+    public Optional<Seat> getSeatByRowAndColumn(int row, int column) {
+        return seats.stream()
+                .filter(s -> s.getRow() == row
+                        && s.getColumn() == column)
+                .findFirst();
+    }
+
+    public Optional<Seat> getSeatByUUID(UUID uuid) {
+        return seats.stream()
+                .filter(s -> uuid.equals(s.getUuid()))
+                .findFirst();
+    }
+}
