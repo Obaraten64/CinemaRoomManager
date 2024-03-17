6c82a326686024920c1c4334942c90cc75248e4c
BuildTools <unconfigured@null.spigotmc.org>
2024-03-17 15:50:48 +0200

    test: added tests for GET /stats endpoint(4/4) and update previous

--- src/test/java/cinemaroommanager/CinemaRepositoryTest.java
+++ src/test/java/cinemaroommanager/CinemaRepositoryTest.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mockStatic;
 
 public class CinemaRepositoryTest {
 
@@ -27,36 +26,85 @@ public class CinemaRepositoryTest {
         assertThat(cinemaRepository.getColumns()).isEqualTo(9);
         assertThat(cinemaRepository.getSeats())
                 .hasSize(81)
-                .hasOnlyElementsOfType(Seat.class);
+                .hasOnlyElementsOfType(Seat.class)
+                .allMatch(s -> s.getColumn() > 0 && s.getColumn() < 10 && s.getRow() > 0
+                        && s.getRow() < 10 && (s.getPrice() == 10 || s.getPrice() == 8));
     }
 
     @Test
     void testGetSeatByRowAndColumn() {
         assertThat(cinemaRepository.getSeatByRowAndColumn(1, 1))
                 .isPresent()
-                .map(s -> List.of(s.getRow(), s.getColumn(), s.getPrice()))
-                .contains(List.of(1, 1, 10));
+                .get()
+                .matches(s -> s.getRow() == 1 && s.getColumn() == 1);
+    }
+
+    @Test
+    void testGetSeatByRowAndColumn_OutOfBounds() {
         assertThat(cinemaRepository.getSeatByRowAndColumn(15, 1)).isNotPresent();
         assertThat(cinemaRepository.getSeatByRowAndColumn(1, 15)).isNotPresent();
     }
 
     @Test
     void testGetSeatByUUID() {
-        try (var mock = mockStatic(UUID.class)) {
-            mock.when(UUID::randomUUID).thenReturn(new UUID(1, 10));
+        Seat seat = cinemaRepository.getSeats().get(2);
+        seat.purchaseSeat();
+        UUID uuid = seat.getUuid();
 
-            Seat seat = cinemaRepository.getSeats().get(2);
-            seat.purchaseSeat();
-            UUID uuid = seat.getUuid();
+        assertThat(cinemaRepository.getSeatByUUID(uuid))
+                .isPresent()
+                .get()
+                .matches(s -> s.getRow() == 1 && s.getColumn() == 3);
+    }
 
-            assertThat(cinemaRepository.getSeatByUUID(uuid))
-                    .isPresent()
-                    .map(s -> List.of(s.getRow(), s.getColumn(), s.getPrice()))
-                    .contains(List.of(1, 3, 10));
-        }
-
+    @Test
+    void testGetSeatByUUID_WrongToken() {
         UUID uuidFromString = UUID.fromString("409548d1-2f6b-4180-8f70-5800c77c17a8");
         assertThat(cinemaRepository.getSeatByUUID(uuidFromString))
                 .isNotPresent();
+    }
+
+    @Test
+    void testGetAvailableSeats() {
+        List<Seat> seats = cinemaRepository.getAvailableSeats();
+        assertThat(seats)
+                .hasOnlyElementsOfType(Seat.class)
+                .hasSize(81)
+                .allMatch(s -> !s.isPurchased());
+    }
+
+    @Test
+    void testGetAvailableSeats_WhenTwoSeatsPurchased() {
+        Seat seat1 = cinemaRepository.getSeats().get(0);
+        Seat seat2 = cinemaRepository.getSeats().get(80);
+        seat1.purchaseSeat();
+        seat2.purchaseSeat();
+
+        List<Seat> seats = cinemaRepository.getAvailableSeats();
+        assertThat(seats)
+                .hasOnlyElementsOfType(Seat.class)
+                .hasSize(79)
+                .noneMatch(e -> (e.getRow() == 1 && e.getColumn() == 1) || (e.getRow() == 9 && e.getColumn() == 9));
+    }
+
+    @Test
+    void testGetPurchasedSeats() {
+        List<Seat> seats = cinemaRepository.getPurchasedSeats();
+        assertThat(seats)
+                .hasSize(0);
+    }
+
+    @Test
+    void testGetPurchasedSeats_WhenTwoSeatsPurchased() {
+        Seat seat1 = cinemaRepository.getSeats().get(0);
+        Seat seat2 = cinemaRepository.getSeats().get(80);
+        seat1.purchaseSeat();
+        seat2.purchaseSeat();
+
+        List<Seat> seats = cinemaRepository.getPurchasedSeats();
+        assertThat(seats)
+                .hasOnlyElementsOfType(Seat.class)
+                .hasSize(2)
+                .allMatch(s -> (s.getRow() == 1 && s.getColumn() == 1) || (s.getRow() == 9 && s.getColumn() == 9));
     }
 }
