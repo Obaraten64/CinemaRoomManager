c14873a08cffa2f9850b5a089303c80d394aba09
BuildTools <unconfigured@null.spigotmc.org>
2024-04-04 18:09:24 +0300

    style: update TODO list

--- src/main/java/cinemaroommanager/CinemaRoomManagerApplication.java
+++ src/main/java/cinemaroommanager/CinemaRoomManagerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CinemaRoomManagerApplication {
-    //TODO: Remove commented lines from Controller and Service, look for the way of throwing exception when user is unauthorized
+    //TODO: Add validation to dto's
     //TODO: Add ability to work with database instead of inmemory
     public static void main(String[] args) {
         SpringApplication.run(CinemaRoomManagerApplication.class, args);
